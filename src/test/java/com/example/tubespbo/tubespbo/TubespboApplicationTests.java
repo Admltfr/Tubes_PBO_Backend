9ceb22095335e225923d98e7fb1716f654ab3ed8
admltfr <adamlutfiramadhan@gmail.com>
2025-05-12 23:18:29 +0700

    initial commit

---
+++ src/test/java/com/example/tubespbo/tubespbo/TubespboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tubespbo.tubespbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubespboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
