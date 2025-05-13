7f3efefffeb0efec32b10db5aebff40a05ec144e
admltfr <adamlutfiramadhan@gmail.com>
2025-05-13 13:28:50 +0700

    feat : connect to database

---
+++ src/main/java/com/example/tubespbo/tubespbo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.tubespbo.tubespbo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.tubespbo.tubespbo.entity.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+}
