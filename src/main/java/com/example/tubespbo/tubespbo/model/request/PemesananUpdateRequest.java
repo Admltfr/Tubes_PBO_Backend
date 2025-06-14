0f8f42ca967792a2ba28faea672c26b39cfa5f83
admltfr <adamlutfiramadhan@gmail.com>
2025-06-14 17:35:06 +0700

    feat: role based access frontend + fix: some adjustments

--- src/main/java/com/example/tubespbo/tubespbo/model/request/PemesananUpdateRequest.java
+++ src/main/java/com/example/tubespbo/tubespbo/model/request/PemesananUpdateRequest.java
@@ -1,9 +1,6 @@
 package com.example.tubespbo.tubespbo.model.request;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
